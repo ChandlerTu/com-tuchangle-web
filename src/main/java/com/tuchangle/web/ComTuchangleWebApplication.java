72201a149dfbce64e941a0e61a306ec3a25d3bf4
Chandler Tu <chandlertu@chandlertu.com>
2018-09-21 17:28:22 +0800

    New Spring Boot project

---
+++ src/main/java/com/tuchangle/web/ComTuchangleWebApplication.java
@@ -0,0 +1,12 @@
+package com.tuchangle.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComTuchangleWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComTuchangleWebApplication.class, args);
+	}
+}
