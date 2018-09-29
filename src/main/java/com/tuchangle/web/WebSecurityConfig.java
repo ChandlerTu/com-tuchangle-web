64e1585418b68886f5298442a16c0222d5a5f699
Chandler Tu <chandlertu@chandlertu.com>
2018-09-29 13:58:35 +0800

    Securing a web application

---
+++ src/main/java/com/tuchangle/web/WebSecurityConfig.java
@@ -0,0 +1,34 @@
+package com.tuchangle.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests().antMatchers("/built/**", "/main.css").permitAll().anyRequest()
+        .authenticated().and().formLogin().defaultSuccessUrl("/", true).permitAll().and().logout()
+        .logoutSuccessUrl("/");
+  }
+
+  @Bean
+  @Override
+  public UserDetailsService userDetailsService() {
+    UserDetails user = User.withUsername("user")
+        .password("{bcrypt}$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HZWzG3YB1tlRy.fqvM/BG")
+        .roles("USER").build();
+
+    return new InMemoryUserDetailsManager(user);
+  }
+
+}
